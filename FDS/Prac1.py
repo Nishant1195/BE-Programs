265f8f87c6c3d94982a4e006f8bc1378fad85707
Nishant1195 <nishant8830875196@gmail.com>
2024-09-09 17:36:44 +0530

    Prac1

--- FDS/Prac1.py
+++ FDS/Prac1.py
@@ -33,11 +33,25 @@ def symm_differ(l1,l2):
 
 C = [1,2,3,4,5]
 B = [4,5,6,7]
-F = [8,9,10]
+F = [6,7,8,9,10]
 
+print("\n1) List of students who play both cricket and badminton \n2) List of students who play either cricket or badminton but not both \n3) Number of students who play neither cricket nor badminton \n4) Number of students who play cricket and football but not badminton.")
+while True:
+    inp = int(input("\n\nEnter your choice (By the serial number i.e. 1-4): "))
 
+    if inp == 1:
+        print("List of students who play both cricket and badminton:", intersect(C, B))
 
-print(unio(C,B))
-print(intersect(C,B))
-print(differ(C,B))
-print(symm_differ(C,B))
+    elif inp == 2:
+        print("List of students who play either cricket or badminton but not both:", symm_differ(C, B))
+
+    elif inp == 3:
+        three = unio(C,B)
+        print("Number of students who play neither cricket nor badminton:", differ(F,three))
+
+    elif inp == 4:
+        four = intersect(C, F)
+        print("Number of students who play cricket and football but not badminton:", differ(four, B))
+    else :
+         print("\n\nError : Invalid Choice/Input")
+         break
