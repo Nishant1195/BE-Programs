49192afea1169195e72b43ac9a6b6ffedd3a6d18
Nishant1195 <nishant8830875196@gmail.com>
2025-01-06 18:58:29 +0530

    File Handling

--- DSA/FileHand.cpp
+++ DSA/FileHand.cpp
@@ -3,9 +3,10 @@
 #include<string.h>
 using namespace std;
 
+
 void create(){
 	int RollNo;
-	string Name, Div;
+	string Name, Div, Address;
 	fstream f;
 	int no;
 	f.open("file1.txt", ios::app);
@@ -19,7 +20,9 @@ void create(){
 	cin>>Name;
 	cout<<"Enter Division: ";
 	cin>>Div;
-	f<<RollNo<<" "<<Name<<" "<<Div<<endl;
+	cout<<"Enter Address: ";
+	cin>>Address;
+	f<<RollNo<<" "<<Name<<" "<<Div<<" "<<Address<<endl;
 	}
 	f.close();
 }
@@ -59,13 +62,47 @@ void display(){
 	f.close();
 }
 
-
+void searchStudent() {
+	int RollNo;
+	string Name, Div, Address;
+	int searchRoll;
+	cout << "Enter Roll No to search: ";
+	cin >> searchRoll;
+	
+	fstream f("file1.txt", ios::in);
+	bool found = false;
+	
+	while(f >> RollNo >> Name >> Div >> Address) {
+		if(searchRoll == RollNo) {
+			cout << "\nStudent found!\n";
+			cout << "Roll No: " << RollNo << "\nName: " << Name 
+				 << "\nDivision: " << Div << "\nAddress: " << Address << endl;
+			found = true;
+			break;
+		}
+	}
+	
+	if(!found) {
+		cout << "Student record not found!" << endl;
+	}
+	f.close();
+}
 
 int main(){
-
-	create();
-	
-	display();
-	
-	remove();
+	int choice;
+	do {
+		cout << "\n1. Add Student\n2. Delete Student\n3. Search Student\n4. Display All\n5. Exit\n";
+		cout << "Enter choice: ";
+		cin >> choice;
+		
+		switch(choice) {
+			case 1: create(); break;
+			case 2: remove(); break;
+			case 3: searchStudent(); break;
+			case 4: display(); break;
+			case 5: cout << "Exiting...\n"; break;
+			default: cout << "Invalid choice!\n";
+		}
+	} while(choice != 5);
+	return 0;
 }
